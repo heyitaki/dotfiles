4d891e39ee784781a4ed22b9ea4b275003794394
aki <akshath@live.com>
2022-08-06 22:38:35 -0500

    add sources

--- setup_dot.sh
+++ setup_dot.sh
@@ -3,7 +3,8 @@
 # Create `$HOME/bin` if it does not already exist
 mkdir -p $HOME/bin
 
-# Add `$HOME/bin` to $PATH if it has not already been added
+# Add `$HOME/bin` to $PATH if it has not already been added: 
+# https://unix.stackexchange.com/a/217626/153872
 [[ ":$PATH:" != *":$HOME/bin"* ]] && PATH="$HOME/bin:${PATH}"
 
 # Get absolute path to this directory: https://stackoverflow.com/a/4774063
