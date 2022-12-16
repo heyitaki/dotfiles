1ca93ae5c30c44d1b104c35a8c61ba1822cebd00
aki <akshath@live.com>
2022-12-16 15:28:15 +0000

    jump: add flag to restart minikube too

--- jump/restart_tilt.sh
+++ jump/restart_tilt.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 
-# This script will restart tilt with the same flags as the last time it was run.
-# TODO: handle restarting minikube too, maybe with a flag?
+# Parse short/long flags
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -m|--minikube)
+      MINIKUBE=true
+      shift
+      ;;
+    -h|--help)
+cat << EOF
+Restart Tilt with the same flags as the last time it was run.
+Usage: restart_tilt [options]
+
+Options:
+  -m, --minikube  Restart minikube before restarting tilt
+  -h, --help      Show this help message
+EOF
+      exit 0
+      ;;
+    *)
+      echo "Unknown flag: $1"
+      exit 1
+      ;;
+  esac
+done
 
 # Access `history` from script
 HISTFILE=~/.bash_history
@@ -11,4 +33,10 @@ set -o history
 tilt_up=$( history | egrep "tilt up -- (--[a-z]+)+" | tail -n 1 | sed -E "s/.*(tilt up -- .*)/\1/" )
 tilt_down=$( echo $tilt_up | sed -E "s/tilt up/tilt down/" )
 eval $tilt_down
+
+if [ "$MINIKUBE" = true ]; then
+  minikube delete
+  minikube start --driver=docker --kubernetes-version=v1.22.3 --cpus=$(nproc) --memory=50G --disk-size=120g --namespace=wormhole
+fi
+
 eval $tilt_up
